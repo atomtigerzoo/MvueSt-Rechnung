dda4b148cd076d863bccd8987368b3cd476ac39e
Henning <stein@atomtigerzoo.com>
2024-09-21 11:51:14 +0200

    lint: removes unused var

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref } from 'vue'
 import currency from 'currency.js'
 
 // User-given values
@@ -55,7 +55,6 @@ function updateTaxRate() {
     calculateNet()
   }
 }
-
 </script>
 
 <template>
