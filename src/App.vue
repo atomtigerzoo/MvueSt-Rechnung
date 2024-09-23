692ab71fafafefd848d2b9151f62bcf922803274
Henning <stein@atomtigerzoo.com>
2024-09-23 15:23:04 +0200

    other: removes commented out code

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,6 @@ import currency from 'currency.js'
 import EntryRow from './components/EntryRow.vue'
 import GrossField from './components/GrossField.vue'
 
-// // User-given values
-// const userNet = ref()
-// const userGross = ref()
-// // Calculated tax amount
-// // const taxAmount = ref(0)
-
-// // Which input was last used?
-// let lastUserInput = 'net'
 
 const taxRates = [
   "7",
@@ -31,37 +23,9 @@ function calcTax(val, tax_rate) {
   return currency(val).multiply(tax_rate / 100)
 }
 
-// function calcTaxFromGross(val, tax_rate) {
-//   return currency(val).subtract(currency(val).divide(1 + tax_rate / 100))
-// }
-
 function convertToHumanAmount(val) {
   return currency(val, outputFormatFullMoney).format()
 }
-
-// function calculate() {
-//   lastUserInput = 'net'
-
-//   const money = convertToCurrency(userNet.value)
-//   // taxAmount.value = calcTax(money, taxRate.value)
-//   // userGross.value = currency(money).add(taxAmount.value).format(inputFieldCurrencyFormat)
-// }
-
-// function calculateNet() {
-//   lastUserInput = 'gross'
-
-//   const money = convertToCurrency(userGross.value)
-//   taxAmount.value = calcTaxFromGross(money, taxRate.value)
-//   userNet.value = currency(money).subtract(taxAmount.value).format(inputFieldCurrencyFormat)
-// }
-
-// function updateTaxRate() {
-//   if (lastUserInput === 'net') {
-//     calculate()
-//   } else {
-//     calculateNet()
-//   }
-// }
 
 
 /**
@@ -210,32 +174,5 @@ function removeEntryRow(id) {
     
     <GrossField :inputValue="convertToHumanAmount(grossTotal)" inputName="grossTotal" label="Gesamt Brutto" />
     
-    <!-- <div class="mx-auto flex justify-center items-center space-x-2 max-lg:flex-col">
-      <input
-        name="userNet"
-        @input="calculate"
-        v-model.trim="userNet"
-        placeholder="Nettobetrag"
-        >
-
-      <span class="text-lg">&plus;</span>
-
-      <select @change="updateTaxRate" v-model="taxRate" name="taxRate">
-        <option disabled>Bitte wählen&hellip;</option>
-        <option value="7">7%</option>
-        <option value="19">19%</option>
-        <option value="20">20% (AT)</option>
-      </select>
-
-      <span class="text-lg">=</span>
-
-      <input
-        name="userGross"
-        @input="calculateNet"
-        v-model="userGross"
-        placeholder="Bruttobetrag"
-        >
-    </div> -->
-
   </div>
 </template>
